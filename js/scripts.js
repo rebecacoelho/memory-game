b983b1e81274ce672a1980949035f29f0eea5e90
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2023-02-24 14:20:46 -0300

    memory-game-completed

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkEndGame() {
+  const flippedCards = document.querySelectorAll(".flip")
+
+  if (flippedCards.length == 12) {
+    alert ("Parabéns! Você concluiu o jogo!")
+  }
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.chemistry === secondCard.dataset.chemistry;
+
+  isMatch ? disableCards() : unflipCards();
+
+  setTimeout(() => {
+    checkEndGame()
+  }, 1000)
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
